0e2661485e172999d6be78c0cfc9bb3a64e76535
Googler <noreply@google.com>
2023-04-04 19:15:36 +0000

    Project import generated by Copybara.
    PiperOrigin-RevId: 521831664

--- __init__.py
+++ __init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Medical research foundations."""
+"""Medical ai research foundations."""
